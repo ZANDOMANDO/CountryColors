1fe4ea3cb68ed402c138cc917a0b4064808798a5
ibBenwah <garret.c.benoit@gmail.com>
2022-08-25 21:52:35 -0400

    Created Country Controller Class with CountryAction(), WelcomeAction(), and GetSelection() methods

--- CountryMVCLab/Country.cs
+++ CountryMVCLab/Country.cs
@@ -8,6 +8,13 @@ namespace CountryMVCLab
 {
     public class Country
     {
+        public Country(string name, string continent, List<string> colors)
+        {
+            Name = name;
+            Continent = continent;
+            Colors = colors;
+        }
+
         public string Name { get; set; }
         public string Continent{ get; set; }
         public List<string> Colors { get; set; }
---
+++ CountryMVCLab/CountryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryMVCLab
+{
+    public class CountryController
+    {
+
+
+
+
+        public List<Country> CountryDb { get; set; } = new List<Country>()
+        {
+            new Country("USA",
+                "North America",
+                new List<string> {"red", "White", "Blue" })
+        };
+
+        public void CountryAction(Country c)
+        {
+            var country = new CountryView(c);
+            country.Display();
+            
+        }
+
+        public void WelcomeAction()
+        {
+            int selection = 
+            Console.WriteLine("Hello, welcome to the country app.Please select a country from the following list: ");
+            var country = new CountryListView(CountryDb);
+            country.Display();
+        }
+
+        public int GetSelection()
+        {
+            Console.WriteLine("Which dish would you like to view? Enter 1-" + CountryDb.Count);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
