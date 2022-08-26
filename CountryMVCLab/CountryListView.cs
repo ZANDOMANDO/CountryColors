95630ea5c8e31ecb3aa43940fc79544bf71846b6
Eddy <eduardo.di.garcia@gmail.com>
2022-08-25 21:32:22 -0400

    Added the CountryListView

---
+++ CountryMVCLab/CountryListView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CountryMVCLab
+{
+    public class CountryListView
+    {
+        public CountryListView(List<Country> countries)
+        {
+            Countries = countries;
+
+        }
+        public List<Country> Countries { get; set; }
+
+        public void Display()
+        {
+
+            for (int i=0; i< Countries.Count(); i++)
+            {
+                Console.WriteLine($"{i+1} :  {Countries[i].Name}");
+            }
+        }
+
+
+
+    }
+}
+
