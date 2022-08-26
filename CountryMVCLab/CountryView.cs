47227efca0c22d3c77879c933cb2f945d8d7fb1b
MacobJagyar <jacobmagyar95@gmail.com>
2022-08-25 21:23:06 -0400

    Created Country view class and Country model

---
+++ CountryMVCLab/CountryView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryMVCLab
+{
+    public class CountryView
+    {
+        public CountryView(Country displayCountry)
+        {
+            DisplayCountry = displayCountry;
+        }
+
+        public Country DisplayCountry { get; set; }
+        public void Display()
+        {
+            string color = "";
+            DisplayCountry.Colors.ForEach(x => color += ", ");
+
+            Console.WriteLine("Name: " + DisplayCountry.Name);
+            Console.WriteLine("Continent: " + DisplayCountry.Continent);
+            Console.WriteLine("Color: " + color);
+        }
+    }
+}
